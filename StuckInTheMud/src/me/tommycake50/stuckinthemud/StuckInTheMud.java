d56d7f9316402fd71b9d1dd267a8103f218a62a0
tommycake50 <tommycake50@server.fake>
2013-07-20 21:44:52 +0100

    Initial commit

---
+++ StuckInTheMud/src/me/tommycake50/stuckinthemud/StuckInTheMud.java
@@ -0,0 +1,10 @@
+package me.tommycake50.stuckinthemud;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class StuckInTheMud extends JavaPlugin {
+	@Override
+	public void onEnable() {
+		
+	}
+}
